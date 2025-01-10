155376540f21e3364d6253fccc4d1913f9ed11e0
patrickMaureal <janpatrickmaureal@gmail.com>
2025-01-10 17:56:59 +0800

    wip:store ui

--- resources/js/Pages/Goal/Dashboard.tsx
+++ resources/js/Pages/Goal/Dashboard.tsx
@@ -1,6 +1,19 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout'
-import { Box, Heading } from '@chakra-ui/react'
+import { Box, Heading, Input, Stack } from '@chakra-ui/react'
 import { Head } from '@inertiajs/react'
+import {
+	DialogActionTrigger,
+	DialogBody,
+	DialogCloseTrigger,
+	DialogContent,
+	DialogFooter,
+	DialogHeader,
+	DialogRoot,
+	DialogTitle,
+	DialogTrigger,
+} from "@/Components/chakra/dialog"
+import { Button } from '@/Components/chakra/button'
+import { Field } from '@/Components/chakra/field'
 
 const GoalDashboard = () => {
 	return (
@@ -9,19 +22,49 @@ const GoalDashboard = () => {
 				<Heading size="xl" fontWeight='semibold'>Goal</Heading>
 			}
 		>
-		
-		<Head title="Goal Dashboard" />
-		
-		<Box paddingY={12}>
-			<Box marginX={'auto'} maxWidth={'7xl'} paddingX={{ sm: 6, lg: 8 }}>
-				<Box overflow={'hidden'} background={'white'} boxShadow={'sm'} rounded={{ sm: 'lg' }}> 
-					<Box p={6}>
-						<Heading size="md" fontWeight="semibold" as="h2">Goal Dashboard</Heading>
-						<p>Welcome to your goal dashboard.</p>
+
+			<Head title="Goal Dashboard" />
+
+			<Box paddingY={12}>
+				<Box marginX={'auto'} maxWidth={'7xl'} paddingX={{ sm: 6, lg: 8 }}>
+					<Box overflow={'hidden'} background={'white'} boxShadow={'sm'} rounded={{ sm: 'lg' }}>
+						<Box p={6}>
+							<DialogRoot>
+								<DialogTrigger asChild>
+									<Button variant="outline" size="sm">
+										Add New Goal
+									</Button>
+								</DialogTrigger>
+								<DialogContent>
+									<DialogHeader>
+										<DialogTitle>Goal</DialogTitle>
+									</DialogHeader>
+									<DialogBody>
+									<Stack gap="8" maxW="sm" css={{ "--field-label-width": "96px" }}>
+										<Field orientation="horizontal" label="Name">
+											<Input placeholder="John Doe" flex="1" />
+										</Field>
+										<Field orientation="horizontal" label="Email">
+											<Input placeholder="me@example.com" flex="1" />
+										</Field>
+										<Field orientation="horizontal" label="Target Amount">
+											<Input placeholder="100.00" flex="1" type='number' pattern='^\d*(\.\d{0,2})?$'/>
+										</Field>
+									</Stack>
+									</DialogBody>
+									<DialogFooter>
+										<DialogActionTrigger asChild>
+											<Button variant="outline">Cancel</Button>
+										</DialogActionTrigger>
+										<Button>Save</Button>
+									</DialogFooter>
+									<DialogCloseTrigger />
+								</DialogContent>
+							</DialogRoot>
+						</Box>
 					</Box>
 				</Box>
 			</Box>
-		</Box>
 
 		</AuthenticatedLayout>
 	)
