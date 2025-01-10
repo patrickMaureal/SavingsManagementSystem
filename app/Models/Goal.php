cc5e25ca1739be591a2d51f7b728b5108ada6e4f
patrickMaureal <janpatrickmaureal@gmail.com>
2025-01-10 10:43:11 +0800

    add relationnship and migration for goal

---
+++ app/Models/Goal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Goal extends Model
+{
+  use HasUuids, SoftDeletes;
+
+	protected $fillable = ['name', 'user_id', 'target_amount', 'start_date', 'end_date'];	
+
+	public function user(): BelongsTo
+	{
+		return $this->belongsTo(User::class);
+	}
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -48,4 +49,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+		public function goals(): HasMany
+		{
+			return $this->hasMany(Goal::class);
+		}
 }
